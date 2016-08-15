88e0ed8dbfd35997aa2913e9713ec862006b8066
mapsam <matthews.sam@gmail.com>
2016-08-15 10:38:13 -0500

    full include path

--- include_dirs.js
+++ include_dirs.js
@@ -1,2 +1,2 @@
 var path = require('path');
-console.log(path.join(path.relative('.', __dirname),'include'));
+console.log(path.join(path.relative('.', __dirname),'include','mapbox','vector_tile'));
