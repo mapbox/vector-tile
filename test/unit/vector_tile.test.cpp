6ac6703b1f98e68e153e83897de0adef85c5c3f7
Dane Springmeyer <dane@mapbox.com>
2016-11-07 15:31:57 -0800

    Add tests for more geometry types

--- test/unit/vector_tile.test.cpp
+++ test/unit/vector_tile.test.cpp
@@ -2,6 +2,7 @@
 #include <mapbox/vector_tile/version.hpp>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
@@ -52,34 +53,91 @@ static std::string open_tile(std::string const& path) {
     return message;
 }
 
-TEST_CASE( "Read tile" ) {
+#define ASSERT_KNOWN_FEATURE() \
+    const auto layer_names = tile.layerNames(); \
+    REQUIRE(layer_names.size() == 1); \
+    REQUIRE(layer_names[0] == "layer_name"); \
+    auto layer_ptr = tile.getLayer("layer_name"); \
+    REQUIRE(layer_ptr != nullptr); \
+    const mapbox::vector_tile::layer & layer = *layer_ptr; \
+    REQUIRE(layer.featureCount() == 1); \
+    REQUIRE(layer.getName() == "layer_name"); \
+    auto feature_ptr = layer.getFeature(0); \
+    const mapbox::vector_tile::feature & feature = * feature_ptr; \
+    auto const& feature_id = feature.getID(); \
+    REQUIRE(feature_id); \
+    REQUIRE(feature_id->is<uint64_t>()); \
+    REQUIRE(feature_id->get<uint64_t>() == 123ull); \
+    auto props = feature.getProperties(); \
+    auto itr = props.find("hello"); \
+    REQUIRE(itr != props.end()); \
+    auto const& val = itr->second; \
+    REQUIRE(val.is<std::string>()); \
+    REQUIRE(val.get<std::string>() == "world"); \
+    auto opt_val = feature.getValue("hello"); \
+    REQUIRE(opt_val); \
+    REQUIRE(opt_val->is<std::string>()); \
+    REQUIRE(opt_val->get<std::string>() == "world"); \
+    mapbox::vector_tile::points_arrays_type geom = feature.getGeometries<mapbox::vector_tile::points_arrays_type>(1.0);
+
+std::string stringify_geom(mapbox::vector_tile::points_arrays_type const& geom) {
+    std::stringstream s;
+    for (auto const& point_array : geom) {
+        s << "[";
+        for (auto const& point : point_array) {
+            s << point.x << ", " << point.y;
+        }
+        s << "]";
+    }
+    return s.str();
+}
+
+TEST_CASE( "Read Feature-single-point.mvt" ) {
     std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-point.mvt");
     mapbox::vector_tile::buffer tile(buffer);
-    const auto layer_names = tile.layerNames();
-    REQUIRE(layer_names.size() == 1);
-    REQUIRE(layer_names[0] == "layer_name");
-    auto layer_ptr = tile.getLayer("layer_name");
-    REQUIRE(layer_ptr != nullptr);
-    const mapbox::vector_tile::layer & layer = *layer_ptr;
-    REQUIRE(layer.featureCount() == 1);
-    REQUIRE(layer.getName() == "layer_name");
-    auto feature_ptr = layer.getFeature(0);
-    const mapbox::vector_tile::feature & feature = * feature_ptr;
-    auto const& feature_id = feature.getID();
-    REQUIRE(feature_id);
-    REQUIRE(feature_id->is<uint64_t>());
-    REQUIRE(feature_id->get<uint64_t>() == 123ull);
-    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::POINT);
-    auto props = feature.getProperties();
-    auto itr = props.find("hello");
-    REQUIRE(itr != props.end());
-    auto const& val = itr->second;
-    REQUIRE(val.is<std::string>());
-    REQUIRE(val.get<std::string>() == "world");
-    auto opt_val = feature.getValue("hello");
-    REQUIRE(opt_val);
-    REQUIRE(opt_val->is<std::string>());
-    REQUIRE(opt_val->get<std::string>() == "world");
+    ASSERT_KNOWN_FEATURE()
+    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::POINT); \
+    REQUIRE(stringify_geom(geom) == "[25, 17]");
 }
+
+TEST_CASE( "Read Feature-single-multipoint.mvt" ) {
+    std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-multipoint.mvt");
+    mapbox::vector_tile::buffer tile(buffer);
+    ASSERT_KNOWN_FEATURE()
+    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::POINT); \
+    REQUIRE(stringify_geom(geom) == "[5, 7][3, 2]");
+}
+
+TEST_CASE( "Read Feature-single-linestring.mvt" ) {
+    std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-linestring.mvt");
+    mapbox::vector_tile::buffer tile(buffer);
+    ASSERT_KNOWN_FEATURE()
+    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::LINESTRING); \
+    REQUIRE(stringify_geom(geom) == "[2, 22, 1010, 10]");
+}
+
+TEST_CASE( "Read Feature-single-multilinestring.mvt" ) {
+    std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-multilinestring.mvt");
+    mapbox::vector_tile::buffer tile(buffer);
+    ASSERT_KNOWN_FEATURE()
+    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::LINESTRING); \
+    REQUIRE(stringify_geom(geom) == "[2, 22, 1010, 10][1, 13, 5]");
+}
+
+TEST_CASE( "Read Feature-single-polygon.mvt" ) {
+    std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-polygon.mvt");
+    mapbox::vector_tile::buffer tile(buffer);
+    ASSERT_KNOWN_FEATURE()
+    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::POLYGON); \
+    REQUIRE(stringify_geom(geom) == "[3, 68, 1220, 343, 6]");
+}
+
+/*TEST_CASE( "Read Feature-single-multipolygon.mvt" ) {
+    std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-multipolygon.mvt");
+    mapbox::vector_tile::buffer tile(buffer);
+    ASSERT_KNOWN_FEATURE()
+    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::POINT); \
+    REQUIRE(stringify_geom(geom) == "25, 17");
+}*/
 
 
