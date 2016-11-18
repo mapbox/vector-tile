e2edd6c0e63fab508d1e43b9ced957d0d55b7116
Blake Thompson <flippmoke@gmail.com>
2016-11-18 12:08:57 -0600

    Added a lot more detail to the encoding feature test

--- test/unit/encoding_feature.cpp
+++ test/unit/encoding_feature.cpp
@@ -9,6 +9,55 @@
 //
 // Unit tests for encoding of features
 //
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wfloat-equal"
+inline void test_property_encoding(protozero::pbf_reader & l,
+                                   mapbox::geometry::property_map & properties) {
+    REQUIRE(l.next());
+    REQUIRE(l.tag() == mapbox::vector_tile::layer_message::KEYS);
+    auto itr = properties.find(l.get_string());
+    REQUIRE(itr != properties.end());
+    REQUIRE(l.next());
+    REQUIRE(l.tag() == mapbox::vector_tile::layer_message::VALUES);
+    auto v = l.get_message();
+    REQUIRE(v.next());
+    switch (v.tag()) {
+        case mapbox::vector_tile::value_message::STRING:
+            REQUIRE(itr->second.is<std::string>());
+            REQUIRE(v.get_string() == itr->second.get<std::string>());
+            break;
+        case mapbox::vector_tile::value_message::FLOAT:
+            REQUIRE(itr->second.is<double>());
+            REQUIRE(static_cast<double>(v.get_float()) == itr->second.get<double>());
+            break;
+        case mapbox::vector_tile::value_message::DOUBLE:
+            REQUIRE(itr->second.is<double>());
+            REQUIRE(v.get_double() == itr->second.get<double>());
+            break;
+        case mapbox::vector_tile::value_message::INT:
+            REQUIRE(itr->second.is<std::int64_t>());
+            REQUIRE(v.get_int64() == itr->second.get<std::int64_t>());
+            break;
+        case mapbox::vector_tile::value_message::UINT:
+            REQUIRE(itr->second.is<std::uint64_t>());
+            REQUIRE(v.get_uint64() == itr->second.get<std::uint64_t>());
+            break;
+        case mapbox::vector_tile::value_message::SINT:
+            REQUIRE(false); // we do not encode things as sint currently
+            break;
+        case mapbox::vector_tile::value_message::BOOL:
+            REQUIRE(itr->second.is<bool>());
+            REQUIRE(v.get_bool() == itr->second.get<bool>());
+            break;
+        default:
+            REQUIRE(false);
+            break;
+    }
+    properties.erase(itr);
+}
+#pragma clang diagnostic pop
+
 TEST_CASE("encode simple point feature") {
 
     mapbox::geometry::point<std::int64_t> point(10,10);
@@ -30,5 +79,39 @@ TEST_CASE("encode simple point feature") {
     mapbox::vector_tile::encode_feature(buffer, layer_keys, layer_values, feature);
     
     protozero::pbf_reader l(buffer);
-    REQUIRE(l.next()); 
+    test_property_encoding(l, properties);
+    test_property_encoding(l, properties);
+    test_property_encoding(l, properties);
+    test_property_encoding(l, properties);
+    test_property_encoding(l, properties);
+    REQUIRE(l.next());
+    REQUIRE(l.tag() == mapbox::vector_tile::layer_message::FEATURES);
+    auto f = l.get_message();
+    REQUIRE(f.next());
+    REQUIRE(f.tag() == mapbox::vector_tile::feature_message::TYPE);
+    REQUIRE(f.get_enum() == mapbox::vector_tile::geom_type::POINT);
+    REQUIRE(f.next());
+    REQUIRE(f.tag() == mapbox::vector_tile::feature_message::GEOMETRY);
+    auto geometry_iter = f.get_packed_uint32();
+    auto itr = geometry_iter.begin();
+    const auto end_itr = geometry_iter.end();
+    
+    // MoveTo, ParameterInteger, ParameterInteger
+    // Therefore 1 commands + 2 parameters = 3
+    REQUIRE(std::distance(itr, end_itr) == 3);
+    // MoveTo(10,10)
+    CHECK(*itr++ == ((1 << 3) | 1u)); // 9
+    CHECK(*itr++ == 20);
+    CHECK(*itr++ == 20);
+    REQUIRE(f.next());
+    REQUIRE(f.tag() == mapbox::vector_tile::feature_message::ID);
+    CHECK(f.get_uint64() == 100);
+    REQUIRE(f.next());
+    REQUIRE(f.tag() == mapbox::vector_tile::feature_message::TAGS);
+    auto tags_iter = f.get_packed_uint32();
+    REQUIRE(std::distance(tags_iter.begin(), tags_iter.end()) == 10);
+    // Not checking that all the tags are correct -- but might be in slightly
+    // different order based on hashing so not specifying in test.
+    REQUIRE_FALSE(f.next());
+    REQUIRE_FALSE(l.next());
 }
