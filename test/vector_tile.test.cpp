b756a6ead6f5344bd4f44151dd835d6bbc156037
Blake Thompson <flippmoke@gmail.com>
2017-10-24 15:29:53 -0500

    Added more tests

--- test/vector_tile.test.cpp
+++ test/vector_tile.test.cpp
@@ -58,41 +58,126 @@ TEST_CASE("Read Feature-single-multipoint.mvt")
     CHECK(val.get<std::string>() == "world");
     REQUIRE(f.geometry.is<mapbox::geometry::multi_point<std::int64_t>>());
     auto mp = f.geometry.get<mapbox::geometry::multi_point<std::int64_t>>();
+    REQUIRE(mp.size() == 2);
     CHECK(mp[0].x == 5);
     CHECK(mp[0].y == 7);
     CHECK(mp[1].x == 3);
     CHECK(mp[1].y == 2);
 }
 
-/*
 TEST_CASE( "Read Feature-single-linestring.mvt" ) {
     std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-linestring.mvt");
-    mapbox::vector_tile::buffer tile(buffer);
-    ASSERT_KNOWN_FEATURE()
-    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::LINESTRING); \
-    REQUIRE(stringify_geom(geom) == "[2, 22, 1010, 10]");
+    auto fm = mapbox::vector_tile::decode_tile<std::int64_t>(buffer);
+    REQUIRE(fm.size() == 1);
+    REQUIRE(fm.size() == 1);
+    REQUIRE(fm.end() != fm.find("layer_name"));
+    auto fc = fm["layer_name"];
+    REQUIRE(fc.size() == 1);
+    auto f = fc[0];
+    REQUIRE(f.id.is<std::uint64_t>());
+    CHECK(f.id.get<std::uint64_t>() == 123);
+    REQUIRE(f.properties.size() == 1);
+    REQUIRE(f.properties.end() != f.properties.find("hello"));
+    auto val = f.properties["hello"];
+    REQUIRE(val.is<std::string>());
+    CHECK(val.get<std::string>() == "world");
+    REQUIRE(f.geometry.is<mapbox::geometry::line_string<std::int64_t>>());
+    auto ls = f.geometry.get<mapbox::geometry::line_string<std::int64_t>>();
+    REQUIRE(ls.size() == 3);
+    CHECK(ls[0].x == 2);
+    CHECK(ls[0].y == 2);
+    CHECK(ls[1].x == 2);
+    CHECK(ls[1].y == 10);
+    CHECK(ls[2].x == 10);
+    CHECK(ls[2].y == 10);
 }
 
 TEST_CASE( "Read Feature-single-multilinestring.mvt" ) {
     std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-multilinestring.mvt");
-    mapbox::vector_tile::buffer tile(buffer);
-    ASSERT_KNOWN_FEATURE()
-    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::LINESTRING); \
-    REQUIRE(stringify_geom(geom) == "[2, 22, 1010, 10][1, 13, 5]");
+    auto fm = mapbox::vector_tile::decode_tile<std::int64_t>(buffer);
+    REQUIRE(fm.size() == 1);
+    REQUIRE(fm.size() == 1);
+    REQUIRE(fm.end() != fm.find("layer_name"));
+    auto fc = fm["layer_name"];
+    REQUIRE(fc.size() == 1);
+    auto f = fc[0];
+    REQUIRE(f.id.is<std::uint64_t>());
+    CHECK(f.id.get<std::uint64_t>() == 123);
+    REQUIRE(f.properties.size() == 1);
+    REQUIRE(f.properties.end() != f.properties.find("hello"));
+    auto val = f.properties["hello"];
+    REQUIRE(val.is<std::string>());
+    CHECK(val.get<std::string>() == "world");
+    REQUIRE(f.geometry.is<mapbox::geometry::multi_line_string<std::int64_t>>());
+    auto mls = f.geometry.get<mapbox::geometry::multi_line_string<std::int64_t>>();
+    REQUIRE(mls.size() == 2);
+    REQUIRE(mls[0].size() == 3);
+    CHECK(mls[0][0].x == 2);
+    CHECK(mls[0][0].y == 2);
+    CHECK(mls[0][1].x == 2);
+    CHECK(mls[0][1].y == 10);
+    CHECK(mls[0][2].x == 10);
+    CHECK(mls[0][2].y == 10);
+    REQUIRE(mls[1].size() == 2);
+    CHECK(mls[1][0].x == 1);
+    CHECK(mls[1][0].y == 1);
+    CHECK(mls[1][1].x == 3);
+    CHECK(mls[1][1].y == 5);
 }
 
 TEST_CASE( "Read Feature-single-polygon.mvt" ) {
     std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-polygon.mvt");
-    mapbox::vector_tile::buffer tile(buffer);
-    ASSERT_KNOWN_FEATURE()
-    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::POLYGON); \
-    REQUIRE(stringify_geom(geom) == "[3, 68, 1220, 343, 6]");
+    auto fm = mapbox::vector_tile::decode_tile<std::int64_t>(buffer);
+    REQUIRE(fm.size() == 1);
+    REQUIRE(fm.size() == 1);
+    REQUIRE(fm.end() != fm.find("layer_name"));
+    auto fc = fm["layer_name"];
+    REQUIRE(fc.size() == 1);
+    auto f = fc[0];
+    REQUIRE(f.id.is<std::uint64_t>());
+    CHECK(f.id.get<std::uint64_t>() == 123);
+    REQUIRE(f.properties.size() == 1);
+    REQUIRE(f.properties.end() != f.properties.find("hello"));
+    auto val = f.properties["hello"];
+    REQUIRE(val.is<std::string>());
+    CHECK(val.get<std::string>() == "world");
+    REQUIRE(f.geometry.is<mapbox::geometry::polygon<std::int64_t>>());
+    auto poly = f.geometry.get<mapbox::geometry::polygon<std::int64_t>>();
+    REQUIRE(poly.size() == 1);
+    REQUIRE(poly[0].size() == 4);
+    CHECK(poly[0][0].x == 3);
+    CHECK(poly[0][0].y == 6);
+    CHECK(poly[0][1].x == 8);
+    CHECK(poly[0][1].y == 12);
+    CHECK(poly[0][2].x == 20);
+    CHECK(poly[0][2].y == 34);
 }
 
+/*
 TEST_CASE( "Read Feature-single-multipolygon.mvt" ) {
     std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-multipolygon.mvt");
-    mapbox::vector_tile::buffer tile(buffer);
-    ASSERT_KNOWN_FEATURE()
-    REQUIRE(feature.getType() == mapbox::vector_tile::GeomType::POINT); \
-    REQUIRE(stringify_geom(geom) == "25, 17");
+    auto fm = mapbox::vector_tile::decode_tile<std::int64_t>(buffer);
+    REQUIRE(fm.size() == 1);
+    REQUIRE(fm.size() == 1);
+    REQUIRE(fm.end() != fm.find("layer_name"));
+    auto fc = fm["layer_name"];
+    REQUIRE(fc.size() == 1);
+    auto f = fc[0];
+    REQUIRE(f.id.is<std::uint64_t>());
+    CHECK(f.id.get<std::uint64_t>() == 123);
+    REQUIRE(f.properties.size() == 1);
+    REQUIRE(f.properties.end() != f.properties.find("hello"));
+    auto val = f.properties["hello"];
+    REQUIRE(val.is<std::string>());
+    CHECK(val.get<std::string>() == "world");
+    REQUIRE(f.geometry.is<mapbox::geometry::multi_polygon<std::int64_t>>());
+    auto mpoly = f.geometry.get<mapbox::geometry::multi_polygon<std::int64_t>>();
+    REQUIRE(mpoly.size() == 2);
+    REQUIRE(mpoly[0].size() == 4);
+    CHECK(mpoly[0][0][0].x == 3);
+    CHECK(mpoly[0][0][0].y == 6);
+    CHECK(mpoly[0][0][1].x == 8);
+    CHECK(mpoly[0][0][1].y == 12);
+    CHECK(mpoly[0][0][2].x == 20);
+    CHECK(mpoly[0][0][2].y == 34);
 }*/
