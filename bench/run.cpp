4b4bd6ee14b7f163bf4f7cc29e293430677cac21
Blake Thompson <flippmoke@gmail.com>
2017-11-30 09:42:24 -0600

    Add benchmarks

--- bench/run.cpp
+++ bench/run.cpp
@@ -1,92 +1,102 @@
-#include <iostream>
-#include <chrono>
+#include <benchmark/benchmark.h>
 #include <mapbox/vector_tile.hpp>
+#include <mapbox/vector_tile/builder.hpp>
+
+#include <limits>
+#include <iostream>
 #include <fstream>
+#include <sstream>
 
-std::size_t feature_count = 0;
-
-static void decode_entire_tile(std::string const& buffer)
+static std::string open_tile(std::string const& path)
 {
-    mapbox::vector_tile::buffer tile(buffer);
-    for (auto const& name : tile.layerNames())
+    std::ifstream stream(path.c_str(), std::ios_base::in | std::ios_base::binary);
+    if (!stream.is_open())
     {
-        const mapbox::vector_tile::layer layer = tile.getLayer(name);
-        std::size_t num_features = layer.featureCount();
-        if (num_features == 0)
-        {
-            std::cout << "Layer '" << name << "' (empty)\n";
-            continue;
-        }
-        for (std::size_t i = 0; i < num_features; ++i)
-        {
-            auto const feature = mapbox::vector_tile::feature(layer.getFeature(i), layer);
-            auto const& feature_id = feature.getID();
-            if (!feature_id)
-            {
-                throw std::runtime_error("Hit unexpected error decoding feature");
-            }
-            auto props = feature.getProperties();
-            mapbox::vector_tile::points_arrays_type geom = feature.getGeometries<mapbox::vector_tile::points_arrays_type>(1.0);
-            ++feature_count;
-        }
+        throw std::runtime_error("could not open: '" + path + "'");
+    }
+    std::string message(std::istreambuf_iterator<char>(stream.rdbuf()), (std::istreambuf_iterator<char>()));
+    stream.close();
+    return message;
+}
+
+template <typename CoordinateType>
+static void BM_decode_polygon_fixture(benchmark::State& state) // NOLINT google-runtime-references
+{
+    std::string buffer = open_tile("test/mvt-fixtures/fixtures/valid/Feature-single-polygon.mvt");
+    vtzero::vector_tile tile(buffer);
+    auto layer = tile.next_layer();
+    auto feature = layer.next_feature();
+
+    while (state.KeepRunning()) {
+        benchmark::DoNotOptimize(mapbox::vector_tile::extract_geometry<CoordinateType>(feature));
     }
 }
 
-static void run_bench(std::vector<std::string> const& tiles, std::size_t iterations)
+template <typename CoordinateType>
+static void BM_decode_polygon(benchmark::State& state) // NOLINT google-runtime-references
 {
-
-    for (std::size_t i = 0; i < iterations; ++i)
+    std::string buffer;
+    std::size_t num_points = 0;
     {
-        for (auto const& tile : tiles)
-        {
-            decode_entire_tile(tile);
+        vtzero::tile_builder tbuilder;
+        std::uint32_t extent = 4096;
+        std::int64_t range = state.range(0);
+        vtzero::layer_builder lbuilder{tbuilder, "my_layer_name", 2, extent};
+        
+        mapbox::geometry::polygon<CoordinateType> poly;
+        poly.emplace_back();
+        mapbox::geometry::linear_ring<CoordinateType> & lr = poly.back();
+        
+        CoordinateType x = static_cast<CoordinateType>(extent / 2) - static_cast<CoordinateType>(range / 2);
+        CoordinateType y = static_cast<CoordinateType>(extent / 2) - static_cast<CoordinateType>(range / 2);
+        for (std::int64_t i = 0; i < range; ++i) {
+            ++num_points;
+            lr.emplace_back(x, y);
+            ++y;
         }
+        for (std::int64_t i = 0; i < range; ++i) {
+            ++num_points;
+            lr.emplace_back(x, y);
+            ++x;
+        }
+        for (std::int64_t i = 0; i < range; ++i) {
+            ++num_points;
+            lr.emplace_back(x, y);
+            --y;
+        }
+        for (std::int64_t i = 0; i < range; ++i) {
+            ++num_points;
+            lr.emplace_back(x, y);
+            --x;
+        }
+        ++num_points;
+        lr.emplace_back(x, y);
+
+        mapbox::geometry::geometry<CoordinateType> geom(std::move(poly));
+        mapbox::feature::identifier id;
+        mapbox::feature::property_map prop;
+        mapbox::feature::feature<CoordinateType> feat{geom, prop, id};
+        mapbox::vector_tile::encode_feature(lbuilder, feat);
+        buffer = tbuilder.serialize();
     }
+
+    vtzero::vector_tile tile(buffer);
+    auto layer = tile.next_layer();
+    auto feature = layer.next_feature();
+
+    while (state.KeepRunning()) {
+        benchmark::DoNotOptimize(mapbox::vector_tile::extract_geometry<CoordinateType>(feature));
+    }
+    // sets a simple counter
+    state.counters["Points"] = num_points;
+    state.counters["PointRate"] = benchmark::Counter(num_points, benchmark::Counter::kIsRate);
 }
 
-template <typename T>
-using milliseconds = std::chrono::duration<T, std::milli>;
+BENCHMARK_TEMPLATE(BM_decode_polygon_fixture, int64_t);
+BENCHMARK_TEMPLATE(BM_decode_polygon_fixture, int32_t);
+BENCHMARK_TEMPLATE(BM_decode_polygon_fixture, int16_t);
+BENCHMARK_TEMPLATE(BM_decode_polygon, int64_t)->RangeMultiplier(2)->Range(1, 1<<14);
+BENCHMARK_TEMPLATE(BM_decode_polygon, int32_t)->RangeMultiplier(2)->Range(1, 1<<14);
+BENCHMARK_TEMPLATE(BM_decode_polygon, int16_t)->RangeMultiplier(2)->Range(1, 1<<14);
 
-int main(/*int argc, char* const argv[]*/)
-{
-    try
-    {
-        std::vector<std::string> tiles;
-        for (std::size_t x = 4680; x <= 4693; ++x)
-        {
-            for (std::size_t y = 6260; y <= 6274; ++y)
-            {
-                std::string path = "bench/mvt-bench-fixtures/fixtures/14-" + std::to_string(x) + "-" + std::to_string(y) + ".mvt";
-                std::ifstream stream(path.c_str(), std::ios_base::in | std::ios_base::binary);
-                if (!stream.is_open())
-                {
-                    throw std::runtime_error("could not open: '" + path + "'");
-                }
-                std::string message(std::istreambuf_iterator<char>(stream.rdbuf()), (std::istreambuf_iterator<char>()));
-                stream.close();
-                tiles.emplace_back(message);
-            }
-        }
-        std::clog << "decoding " << tiles.size() << " tiles\n";
-        std::clog << "warming up...\n";
-        // warmup
-        run_bench(tiles, 1);
-        std::clog << "running bench...\n";
-        // now actually decode all tiles for N iterations
-        auto t1 = std::chrono::high_resolution_clock::now();
-        run_bench(tiles, 100);
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto elapsed = milliseconds<double>(t2 - t1).count();
-        if (feature_count != 8157770)
-        {
-            std::clog << "Warning expected feature_count of 8157770, was: " << feature_count << "\n";
-        }
-        std::clog << "elapsed: " << std::fixed << elapsed << " ms\n";
-    }
-    catch (std::exception const& ex)
-    {
-        std::cerr << ex.what() << "\n";
-        return -1;
-    }
-    return 0;
-}
+BENCHMARK_MAIN();
