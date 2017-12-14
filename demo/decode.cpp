667fe7b1c05f87fe3d595ff0de6e4d724ddc0b59
Dane Springmeyer <dane@mapbox.com>
2017-12-14 15:19:33 -0800

    feature_id is optional in spec to display status rather than throwing - refs mapbox/mapnik-vector-tile#251

--- demo/decode.cpp
+++ demo/decode.cpp
@@ -73,10 +73,11 @@ int main(int argc, char** argv) {
             for (std::size_t i=0;i<feature_count;++i) {
                 auto const feature = mapbox::vector_tile::feature(layer.getFeature(i),layer);
                 auto const& feature_id = feature.getID();
-                if (!feature_id) {
-                    throw std::runtime_error("Hit unexpected error decoding feature");
+                if (feature_id) {
+                    std::cout << "    id: " << (*feature_id).get<uint64_t>() << "\n";
+                } else {
+                    std::cout << "    id: (no id set)\n";
                 }
-                std::cout << "    id: " << (*feature_id).get<uint64_t>() << "\n";
                 std::cout << "    type: " << int(feature.getType()) << "\n";
                 auto props = feature.getProperties();
                 std::cout << "    Properties:\n";
